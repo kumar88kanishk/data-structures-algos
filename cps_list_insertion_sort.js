402c3ade1cae109bc0f16f5d8a6ff76a8b0fea72
Kanishk Kumar <kumar88kanishk@gmail.com>
2024-02-07 12:23:18 +0530

    Insertion sort using lists

---
+++ cps_list_insertion_sort.js
@@ -0,0 +1,58 @@
+// null
+// [1, null]
+// [1, [2, null]]
+// [1, [2, null]]
+// [1, [2, [3, null]]]
+
+// implementing insertion sort using linked list
+
+// fn takes a sorted list array and insert the next item
+function insert(sortedList, e) {
+  if (sortedList === null)
+    return [e, null]
+  else if (e < sortedList[0])
+    return [e, sortedList]
+  else
+    return [sortedList[0], insert(sortedList[1], e)]
+}
+
+// fn walks down the sorted list and returns array
+function walk(list) {
+  let arr = []
+  while (list !== null) {
+    arr.push(list[0])
+    list = list[1]
+  }
+  return arr
+}
+
+// fn takes a unsorted array and picks one element at a time and puts them into a new list
+function sort(arr) {
+  let sorted = null
+
+  for (let e of arr)
+    sorted = insert(sorted, e)
+
+  return walk(sorted)
+}
+
+
+// TEST
+// ==== Fn takes a size and generates random array of the size (0,n) and sorts them
+function test(size) {
+  function shuffle(arr) {
+    return arr
+      .map(value => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value)
+  }
+
+  let arr = [...Array(size).keys()]
+  let shuffled = shuffle(arr)
+  let sorted = sort(shuffled)
+
+  console.log(arr[0] === sorted[0]
+    && arr.at(-1) === sorted.at(-1))
+}
+
+test(100)
