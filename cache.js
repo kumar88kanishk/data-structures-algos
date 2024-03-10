740babcbb3b2705b1bc49a4b52132df135d0cd11
Kanishk Kumar <kumar88kanishk@gmail.com>
2024-03-10 19:50:00 +0530

    Implementing Cache via memoizing fns as representation

--- cache.js
+++ cache.js
@@ -1,21 +1,23 @@
 // 0, 1, 1, 2, 3, 5, 8....
 let cache = {}
-function fib(n) {
 
-  let res;
-  if (cache[n])
-    return cache[n]
-
-  if (n === 0)
-    res = 0
-  else if (n === 1)
-    res = 1
-  else
-    res = fib(n - 2) + fib(n - 1)
-
-  cache[n] = res
-
-  return res
+function memo(f) {
+  return function (x) {
+    if (cache[x])
+      return cache[x]
+    else {
+      cache[x] = f(x)
+      return cache[x]
+    }
+  }
 }
-
+function _fib(n) {
+  if (n === 0)
+    return 0
+  else if (n === 1)
+    return 1
+  else
+    return fib(n - 2) + fib(n - 1)
+}
+let fib = memo(_fib)
 console.log(fib(50))
