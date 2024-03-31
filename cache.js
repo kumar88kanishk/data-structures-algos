0cb9c41c10438728a3f212539a60f798a5ea5d59
Kanishk Kumar <kumar88kanishk@gmail.com>
2024-03-31 18:41:39 +0530

    Fixing cache

--- cache.js
+++ cache.js
@@ -1,19 +1,22 @@
 // 0, 1, 1, 2, 3, 5, 8....
 class LRUCache {
-  // introducing capacity to implement LRU
+  // Capcity limited LRU
   // LRU (Least Recently Used) is a GC(Garbage Collection)
   constructor(capacity) {
     this.store = {}
     this.capacity = capacity
     this.size = 0
   }
+
   put(key, val) {
     if (!this.has(key))
       this.size++
+
     this.store[key] = { ts: Date.now(), val }
     if (this.size > this.capacity) {
       let keys = Object.keys(this.store)
       let firstKey = keys[0]
+
       let leastRecent = keys.slice(1).reduce(([lastKey, lastTs], lruKey) => {
         if (lastTs < this.store[lruKey].ts)
           return [lastKey, lastTs]
@@ -21,13 +24,13 @@ class LRUCache {
           return [lruKey, this.store[lruKey].ts]
       }, [firstKey, this.store[firstKey].ts])
 
-      //console.log(leastRecent, this.store);
+      // console.log(leastRecent[0], this.store);
       delete this.store[leastRecent[0]]
+      this.size--
     }
   }
 
   get(key) {
-    // console.log("key", key, this.store)
     if (this.has(key)) {
       this.store[key].ts = Date.now()
       return this.store[key].val
@@ -39,8 +42,8 @@ class LRUCache {
   }
 }
 
-function memo(f) {
-  let lruCache = new LRUCache(4)
+function memo(f, size = 4) {
+  let lruCache = new LRUCache(size)
   return function (x) {
     if (lruCache.has(x))
       return lruCache.get(x)
@@ -60,4 +63,20 @@ function _fib(n) {
     return fib(n - 2) + fib(n - 1)
 }
 let fib = memo(_fib)
-console.log(fib(8))
+console.log(fib(20))
+
+
+
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// let newCache = new LRUCache(1)
+// newCache.put("key1", "val1")
+// console.log("newCache", newCache)
+// await sleep(5)
+// newCache.put("key2", "val2")
+// console.log("newCache", newCache)
+// await sleep(5)
+// newCache.put("key3", "val3")
+// console.log("newCache", newCache)
